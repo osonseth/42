920bcc9a5010642bb5b18ce1725c2639f6b4ccc3
osonseth <max.cesaria@gmail.com>
2024-04-17 14:00:07 +0200

    last V, so long

--- Cursus/test/mlx2.c
+++ Cursus/test/mlx2.c
@@ -6,13 +6,13 @@
 /*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 11:15:57 by max               #+#    #+#             */
-/*   Updated: 2024/04/17 13:51:11 by max              ###   ########.fr       */
+/*   Updated: 2024/04/17 13:56:39 by max              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	keypress_is_up(t_data *data)
+void keypress_is_up(t_data *data)
 {
 	if (data->map[data->player_y - 1][data->player_x] == '0')
 	{
@@ -25,14 +25,15 @@ void	keypress_is_up(t_data *data)
 		data->map[data->player_y][data->player_x] = '0';
 		data->map[data->player_y - 1][data->player_x] = 'P';
 		data->player_y = data->player_y - 1;
+		data->number_of_collectibles--;
 	}
-	else if (data->map[data->player_y - 1][data->player_x] == 'E')
+	else if (data->map[data->player_y - 1][data->player_x] == 'E' && data->number_of_collectibles == 0)
 		clean_all(data, "END OF THE GAME !");
 	else
-		return ;
+		return;
 }
 
-void	keypress_is_down(t_data *data)
+void keypress_is_down(t_data *data)
 {
 	if (data->map[data->player_y + 1][data->player_x] == '0')
 	{
@@ -45,14 +46,15 @@ void	keypress_is_down(t_data *data)
 		data->map[data->player_y][data->player_x] = '0';
 		data->map[data->player_y + 1][data->player_x] = 'P';
 		data->player_y = data->player_y + 1;
+		data->number_of_collectibles--;
 	}
-	else if (data->map[data->player_y + 1][data->player_x] == 'E')
+	else if (data->map[data->player_y + 1][data->player_x] == 'E' && data->number_of_collectibles == 0)
 		clean_all(data, "END OF THE GAME !");
 	else
-		return ;
+		return;
 }
 
-void	keypress_is_right(t_data *data)
+void keypress_is_right(t_data *data)
 {
 	if (data->map[data->player_y][data->player_x + 1] == '0')
 	{
@@ -65,14 +67,15 @@ void	keypress_is_right(t_data *data)
 		data->map[data->player_y][data->player_x] = '0';
 		data->map[data->player_y][data->player_x + 1] = 'P';
 		data->player_x = data->player_x + 1;
+		data->number_of_collectibles--;
 	}
-	else if (data->map[data->player_y][data->player_x + 1] == 'E')
+	else if (data->map[data->player_y][data->player_x + 1] == 'E' && data->number_of_collectibles == 0)
 		clean_all(data, "END OF THE GAME !");
 	else
-		return ;
+		return;
 }
 
-void	keypress_is_left(t_data *data)
+void keypress_is_left(t_data *data)
 {
 	if (data->map[data->player_y][data->player_x - 1] == '0')
 	{
@@ -85,15 +88,16 @@ void	keypress_is_left(t_data *data)
 		data->map[data->player_y][data->player_x] = '0';
 		data->map[data->player_y][data->player_x - 1] = 'P';
 		data->player_x = data->player_x - 1;
+		data->number_of_collectibles--;
 	}
-	else if (data->map[data->player_y][data->player_x - 1] == 'E')
+	else if (data->map[data->player_y][data->player_x - 1] == 'E' && data->number_of_collectibles == 0)
 		clean_all(data, "END OF THE GAME !");
 	else
-		return ;
+		return;
 }
 
-int close_windows (t_data *data)
+int close_windows(t_data *data)
 {
-	clean_all(data,"You left the game !");
+	clean_all(data, "You left the game !");
 	return 0;
 }
